bba8fba1fc688436461898cd71b3d0deaa86670b
Ben Hughes <benhughes@shaw.ca>
2015-10-25 15:39:13 -0700

    Implemented Direction enum.

--- src/Direction.java
+++ src/Direction.java
@@ -1,4 +1,27 @@
 
 public enum Direction {
+    
+    NORTH(0, "North/Up"),
+    SOUTH(1, "South/Down"),
+    EAST(2, "East/Right"),
+    WEST(3, "West/Down");
+    
+
+    private final int ID;
+    private final String text;
+
+
+    private Direction(final int ID, final String text) {
+        this.ID = ID;
+        this.text = text;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return this.text;
+    }
 
 }
