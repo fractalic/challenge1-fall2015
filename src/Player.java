fcd711963e319d2171279bcde6e6b11a8fa3e256
Ben Hughes <benhughes@shaw.ca>
2015-10-24 23:22:35 -0700

    Updated Player API specs.
    Considering removal of a few functions included in Bader’s original
    version: including low-specificity constructors and non-scalable
    functions.

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,72 @@
 public class Player {
+    
 	
 	public enum PlayerType {
+	    HUMAN(0, "Human player."),
+        BOT(1, "Bot with no intelligence."),
+        BOT_HARD(2, "Bot with more intelligence.");
+        
+
+        private final int ID;
+        private final String text;
+
+
+        private PlayerType(final int ID, final String text) {
+            this.ID = ID;
+            this.text = text;
+        }
+
+        /* (non-Javadoc)
+         * @see java.lang.Enum#toString()
+         */
+        @Override
+        public String toString() {
+            return this.text;
+        }
 
 	}
-	public Player(){}
-	public Player(PlayerType type){}
-	public Player(String name, PlayerType type){}
+	
+	/*
+	 * API
+	 * I think I want to get rid of the blank constructors.
+	 * 
+	 * I want to get rid of getOpponentLocation because that is not scalable.
+	 * I think I want to store the player location on the player, because that
+	 * makes it easy to pass to another level.
+	 * 
+	 * I think I'll set the location directly instead of using moveTo
+	 */
+	//public Player(){}
+	//public Player(PlayerType type){}
+	
+	/**
+	 * Create a Player of a given type with a given name.
+	 * @param name The name of the Player.
+	 * @param type The type of the Player.
+	 * @return an unplaced Player.
+	 */
+	public Player(final String name, final PlayerType type){}
 
+	/**
+	 * Get the location of the Player on the board.
+	 * 
+	 * @return The current location of the Player on the board.
+	 */
 	public Location getLocation(){ return new Location();}
-	public boolean moveTo(Direction direction){ return false; }	
-	public Location getOpponentLocation(){ return new Location(); }
+	
+	/**
+	 * Place the Player on the board.
+	 * 
+	 * @requires location is a valid location on the board.
+	 * 
+	 * @param location is a valid location where the player
+	 *        will be placed.
+	 * @effects The Player updates its location to the given value.
+	 * @modifies Player's location.
+	 */
+	public void setLocation(final Location location) { }
+	
+	//public boolean moveTo(Direction direction){ return false; }	
+	//public Location getOpponentLocation(){ return new Location(); }
 }
 
