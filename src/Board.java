811bd18f880d056401abd96a00459d127cf11901
Ben Hughes <benhughes@shaw.ca>
2015-10-24 23:41:40 -0700

    Added Board constructor and JUnit.

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,19 @@
 import java.util.List;
 
 public class Board {
+    
+    private int dimension = 0;
+    
+    //public static final Location origin = new Location(0,0);
 	
-	public Board(int dimension) {}
+    /**
+     * Create an empty square Board with size
+     * dimension by dimension.
+     * @param dimension The dimension of the Board along one edge.
+     */
+	public Board(int dimension) {
+	    this.dimension = dimension;
+	}
 
 	/* direction is one of North, South, East and West. */
 	List<Direction> getAvailableDirectionsAt(Location location){ return null; }	
---
+++ src/BoardTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+    @Test
+    public void construct() {
+        Board testBoard = new Board(7);
+    }
+
+}
